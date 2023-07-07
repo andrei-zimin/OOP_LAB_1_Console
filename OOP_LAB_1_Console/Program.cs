456fa70281cd20b31d859167028f0af8eb28534d
cherpontov <62599638+cherpontov@users.noreply.github.com>
2023-07-07 03:06:53 +0300

    First Commit
    First Commit

---
+++ OOP_LAB_1_Console/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_LAB_1_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Properties.Settings setings = Properties.Settings.Default;
+            Console.WriteLine(Properties.Resources.ResHello + ", " + setings.Name);
+            Console.ReadLine();
+            Console.WriteLine(Properties.Resources.ResGoodbye + ", " + setings.Name);
+            Console.ReadLine();
+        }
+    }
+}
